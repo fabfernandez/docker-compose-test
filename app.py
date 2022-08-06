497d931ef8f264e2ea36ae988eec6d8d86f19e51
Fabrizio Fernandez <fabrizio.fernandez@mercadolibre.com>
2022-08-06 13:08:45 -0300

    ping and hello

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
+
 def get_hit_count():
     retries = 5
     while True:
@@ -17,7 +18,12 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-@app.route('/')
+
+@app.get('/')
 def hello():
     count = get_hit_count()
     return 'Hello from FABA! I have been seen {} times.\n'.format(count)
+
+@app.get('/ping')
+def hello_world():
+    return 'Pong!'
